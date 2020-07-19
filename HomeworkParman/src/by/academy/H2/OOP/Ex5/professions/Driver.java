cf882a52bc22bc568999ddd75b05905894f451a4
maddx2020 <madd.xx83@gmail.com>
2020-07-19 23:10:39 +0300

    upd

--- HomeworkParman/src/by/academy/H2/OOP/Ex5/professions/Driver.java
+++ HomeworkParman/src/by/academy/H2/OOP/Ex5/professions/Driver.java
@@ -1,5 +1,30 @@
 package by.academy.H2.OOP.Ex5.professions;
 
 public class Driver {
+	
+	private String fullName;
+	private int experience;
+	public Driver() {
+		super();
+	}
+	public Driver(String fullName, int experience) {
+		super();
+		this.fullName = fullName;
+		this.experience = experience;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getExperience() {
+		return experience;
+	}
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	
+	
 
 }
--- HomeworkParman/src/by/academy/H2/OOP/Ex5/vehicles/Car.java
+++ HomeworkParman/src/by/academy/H2/OOP/Ex5/vehicles/Car.java
@@ -1,5 +1,13 @@
 package by.academy.H2.OOP.Ex5.vehicles;
 
-public class Car {
+import by.academy.H2.OOP.Ex5.details.Engine;
+import by.academy.H2.OOP.Ex5.professions.Driver;
 
+public class Car {
+	
+	private String marka;
+	private String carClass;
+	private int weight;
+	private Driver driver;
+	private Engine engine;
 }
