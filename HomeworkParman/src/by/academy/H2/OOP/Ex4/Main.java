b873f69f2ba1b7cb510850b7302d8ed2d1c5179d
maddx2020 <madd.xx83@gmail.com>
2020-07-19 19:36:47 +0300

    mass remove

---
+++ HomeworkParman/src/by/academy/H2/OOP/Ex4/Main.java
@@ -0,0 +1,15 @@
+package by.academy.H2.OOP.Ex4;
+
+import by.academy.Lesson5.OOP.Ex4.Reader;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Reader reader1 = new Reader("Петров В. В.", 143, "ФРЭ", "1 января 2000", 12_13_789);
+
+		reader1.takeBook(4);
+		
+		
+	}
+
+}
