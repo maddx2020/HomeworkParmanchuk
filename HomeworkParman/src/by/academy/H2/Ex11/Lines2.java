55c17bbe3356e5f55ee764bdc21b9a664e61b990
maddx2020 <madd.xx83@gmail.com>
2020-07-22 00:06:33 +0300

    fin upd

---
+++ HomeworkParman/src/by/academy/H2/Ex11/Lines2.java
@@ -0,0 +1,16 @@
+//Условие задачи: ввести 2 слова, состоящие из четного числа букв. 
+//Получить слово состоящее из первой половины первого слова и второй половины второго слова.
+
+package by.academy.H2.Ex11;
+
+public class Lines2 {
+	public static void main(String[] args) {
+
+		String s1 = "Четное";
+		String s2 = "половина";
+
+		System.out.println(s1.substring(0, (s1.length() / 2)).concat(s2.substring(s2.length() / 2)));
+
+	}
+
+}
