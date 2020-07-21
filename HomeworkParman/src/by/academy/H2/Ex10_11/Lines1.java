c2466a76bb8b0485bd691af23a4b8befac2941eb
maddx2020 <madd.xx83@gmail.com>
2020-07-22 02:16:27 +0300

    fin upd

---
+++ HomeworkParman/src/by/academy/H2/Ex10_11/Lines1.java
@@ -0,0 +1,34 @@
+//Ввести n слов с консоли. Найти слово, в котором число различных символов минимально.
+//Если таких слов несколько, найти первое из них.
+
+package by.academy.H2.Ex10_11;
+
+public class Lines1 {
+	public static void main(String[] args) {
+
+		int minIndexOfString = -1;
+		int minCounterSymbols = Integer.MAX_VALUE;
+
+		String[] array = { "змея", "ёж", "черепаха", "Бегемот", "крокодил", "змея", "носорог", "лис", "иж" };
+
+		for (int s = 0; s < array.length; s++) {
+			int counterSymbols = 0;
+			char[] chars = new char[2000];
+			for (char c : array[s].toCharArray()) {
+				chars[c]++;
+			}
+			for (char c : chars) {
+				if (c > 0) {
+					counterSymbols++;
+				}
+			}
+			if (counterSymbols < minCounterSymbols) {
+				minCounterSymbols = counterSymbols;
+				minIndexOfString = s;
+			}
+
+		}
+		System.out.println("Слово, в котором количество различных символов минимально - " + array[minIndexOfString]);
+	}
+
+}
