c71272802e5dec52291466441e0327485550e593
maddx2020 <madd.xx83@gmail.com>
2020-07-22 16:54:59 +0300

    +

--- HomeworkParman/src/by/academy/H2/Ex4.java
+++ HomeworkParman/src/by/academy/H2/Ex4.java
@@ -6,9 +6,32 @@
 
 package by.academy.H2;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Ex4 {
-int n=0;
 
+	public static void main(String[] args) throws ParseException {
 
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String[] array1 = { "01/02/9999", "00/50/0000", "15/12/2019", "20/12/2020", "2015/11/28" };
+
+		for (String a : array1) {
+			Pattern p1 = Pattern.compile("([0]?[1-9]|[1|2][0-9]|[3]|[0|1])/([0]?[1-9]|1[0-2])/\\d{4}");
+			Matcher m1 = p1.matcher(a);
+			if (m1.find()) {
+				System.out.println(m1.group());
+				date = sdf.parse(a);
+				System.out.println("Распаршенная дата - " + date + "\n");
+			} else {
+				System.out.println("Неверная дата" + "\n");
+			}
+		}
+
+	}
 
 }
