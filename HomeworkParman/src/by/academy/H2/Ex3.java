0aef73a1c19a451ef56808786d1afa35a861af75
maddx2020 <madd.xx83@gmail.com>
2020-07-22 03:23:30 +0300

    +

---
+++ HomeworkParman/src/by/academy/H2/Ex3.java
@@ -0,0 +1,9 @@
+//Написать класс, который проверял бы введенную дату на соответствие патерна: (использовать regexp)
+//   a) dd/MM/yyyy
+//   b) dd-MM-yyyy 
+
+package by.academy.H2;
+
+public class Ex3 {
+
+}
