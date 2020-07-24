864219bcd5c9185477eab47f3f112f17bac3b3b0
maddx2020 <madd.xx83@gmail.com>
2020-07-24 23:08:18 +0300

    +

--- HomeworkParman/src/by/academy/H2/Ex5/MainH2Ex5.java
+++ HomeworkParman/src/by/academy/H2/Ex5/MainH2Ex5.java
@@ -1,19 +1,16 @@
 package by.academy.H2.Ex5;
 
-import by.academy.BelarusPhoneValidator;
-
 public class MainH2Ex5 {
 
 	public static void main(String[] args) {
-		
+
 		Validator amer = new AmericanPhoneValidator();
 		Validator bel = new BelarusPhoneValidator();
 		Validator email = new EmailValidator();
-		
+
 		System.out.println(amer.validate("+112345678901"));
 		System.out.println(bel.validate("+375442767438"));
 		System.out.println(email.validate("tut@mozgov.net"));
-		
 
 	}
 
