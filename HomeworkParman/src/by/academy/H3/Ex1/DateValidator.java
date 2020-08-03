6156657ad2a418b42d940be86dc5ac569eacabb6
maddx2020 <madd.xx83@gmail.com>
2020-08-03 04:37:58 +0300

    пора спать

--- HomeworkParman/src/by/academy/H3/Ex1/DateValidator.java
+++
@@ -1,5 +0,0 @@
-package by.academy.H3.Ex1;
-
-public interface DateValidator {
-
-}
---
+++ HomeworkParman/src/by/academy/H3/Ex1/MainDate.java
@@ -0,0 +1,17 @@
+package by.academy.H3.Ex1;
+
+public class MainDate {
+
+	public static void main(String[] args) {
+
+		Date oneDate = new Date("22.08.1950");
+		
+		Date someDate = new Date();
+		
+		someDate.setDate();
+//		System.out.println(someDate.setDate());
+		
+
+	}
+
+}
