22d9978b8c1a35e1c3ebc08f47608911091f8a5d
maddx2020 <madmaxx@tut.by>
2020-08-06 03:28:06 +0200

    test update

--- HomeworkParman/src/by/academy/H3/EX2/MainBox.java
+++ HomeworkParman/src/by/academy/H3/EX2/MainBox.java
@@ -6,7 +6,7 @@ public class MainBox {
 
 	public static void main(String[] args) {
 		Box<Integer> arrayInt = new Box<Integer>(2);
-		System.out.println("Index of last element is - " + arrayInt.getLastIndex());
+		
 		arrayInt.add(79);
 		arrayInt.add(3);
 		arrayInt.add(798);
@@ -15,25 +15,22 @@ public class MainBox {
 
 		arrayInt.add(11);
 		arrayInt.add(43);
-		System.out.println(Arrays.toString(arrayInt.getArray()));
 		System.out.println("Last element is - " + arrayInt.getLast() + "\n");
 
 		System.out.println("Element by index is - " + arrayInt.get(4));
 		System.out.println("First element is - " + arrayInt.getFirst());
+		
 		arrayInt.add(0);
-		System.out.println(Arrays.toString(arrayInt.getArray()));
-		System.out.println("Array length is - " + arrayInt.getLength() + "\n");
-
 		arrayInt.add(24);
 		arrayInt.add(666);
 		System.out.println(Arrays.toString(arrayInt.getArray()));
 		System.out.println("Index of last element is - " + arrayInt.getLastIndex() + "\n");
 
 		arrayInt.removeByIndex(3);
-		System.out.println("Array after del by index - " + Arrays.toString(arrayInt.getArray()));
+		System.out.println("Array after delete by index - " + Arrays.toString(arrayInt.getArray()));
 		System.out.println("Array length is - " + arrayInt.getLength() + "\n");
 
-		arrayInt.removeByItem(43);
+		arrayInt.removeByItem(24);
 		System.out.println("Array after del by Item - " + Arrays.toString(arrayInt.getArray()));
 	}
 
---
+++ HomeworkParman/src/by/academy/H3/Ex1/ValidatorMyDate.java
@@ -0,0 +1,7 @@
+package by.academy.H3.Ex1;
+
+public interface ValidatorMyDate {
+
+	public boolean isValid(String str);
+
+}
