5ae8f89eb993be3e984c3900d423096fe3b2a341
maddx2020 <madmaxx@tut.by>
2020-08-22 21:58:10 +0200

    done

--- HomeworkParman/src/by/academy/H5/Ex4.java
+++ HomeworkParman/src/by/academy/H5/Ex4.java
@@ -1,22 +1,39 @@
 package by.academy.H5;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Ex4 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		File file = new File("Temp");
 		file.mkdir();
-		File file2 = new File("Ex2.txt");
-		long quantity = file2.length();
 
-			for (int i = 1; i <= 5; i++) {
+		StringBuilder sb = new StringBuilder();
+		String line;
+
+		try (BufferedReader br = new BufferedReader(new FileReader("Ex2.txt"))) {
+
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			System.out.println("Something wrong with file - Ex2.txt");
+		}
+
+		line = sb.toString();
+
+		for (int i = 1; i <= 5; i++) {
 			String path = "Temp/" + i + ".txt";
-			try (FileOutputStream fos = new FileOutputStream(path)) {
-				int rand = (int) (Math.random() * quantity + 1);
-				System.out.println(rand);
+			try (FileWriter fw = new FileWriter(path)) {
+				int rand = (int) (Math.random() * line.length() + 1);
+				fw.write(line.substring(0, rand));
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 
 		}
